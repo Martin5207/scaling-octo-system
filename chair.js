e40b8e878e0e82b150cade88d386d170ec5fb74c
Martin5207 <96116454+Martin5207@users.noreply.github.com>
2023-08-21 19:14:02 +0930

    Add files via upload

---
+++ chair.js
@@ -0,0 +1,28 @@
+function setup(){
+    canvas=createCanvas(640,420);
+    canvas.center();
+    objectDetector=ml5.objectDetector("cocossd",modelloaded);
+    
+    }
+    var status="";
+    
+    function modelloaded(){
+        console.log("READY");
+        status=true;
+        document.getElementById("status").innerHTML="status:finding stuff"
+        objectDetector.detect(img,eee)
+    }
+    function eee(error,results){
+     if(error){   console.log(error)
+     }
+    else{
+        console.log(results)
+    }
+    }
+    var img="";
+    function preload(){
+        img=loadImage("chair.jpg");
+    }
+    function draw(){
+        image(img,0,0,640,420);
+    }
---
+++ main.js
@@ -0,0 +1,15 @@
+function bed(){
+    window.location="bed.html"
+}
+function chair(){
+    window.location="chair.html"
+}
+function pets(){
+    window.location="pets.html"
+}
+function salid(){
+    window.location="salid.html"
+}
+function bottles(){
+    window.location="bottles.html"
+}
